b799707dcf023de276f4554e8a342dd18ccc4210
rolson2-pixel <rolson2@stevens.edu>
2026-05-06 22:15:32 -0400

    Create test_main.py

---
+++ test_main.py
@@ -0,0 +1,26 @@
+# This is a file for PyTest cases
+# To run this test, type pytest in the terminal
+import pytest
+
+from main import Movie, User
+
+def test_update():
+  """This test function is for checking the update method in the Movie Class"""
+    movie = Movie("Old Title", "Robert Olson", 1993, "Comedy")
+
+    movie.update(title="New Title", year=2024)
+
+    assert movie.title == "New Title"
+    assert movie.year == 2024
+    assert movie.director == "Someone"
+
+def test_remove():
+  """This function tests is the remove movie method method in the User Class functions properly"""
+    user = User()
+
+    movie = Movie("Batman Begins", "Nolan", 2005, "Action")
+
+    user.add_movie(movie)
+    user.remove_movie(0)
+
+    assert len(user) == 0
