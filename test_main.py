3d69f7f2bd54a07d929f6a4ac3e1b230f264436c
rolson2-pixel <rolson2@stevens.edu>
2026-05-06 23:44:01 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -6,21 +6,21 @@ from main import Movie, User
 
 def test_update():
   """This test function is for checking the update method in the Movie Class"""
-    movie = Movie("Old Title", "Robert Olson", 1993, "Comedy")
+  movie = Movie("Old Title", "Robert Olson", 1993, "Comedy")
 
-    movie.update(title="New Title", year=2024)
+  movie.update(title="New Title", year=2024)
 
-    assert movie.title == "New Title"
-    assert movie.year == 2024
-    assert movie.director == "Someone"
+  assert movie.title == "New Title"
+  assert movie.year == 2024
+  assert movie.director == "Robert Olson"
 
 def test_remove():
   """This function tests is the remove movie method method in the User Class functions properly"""
-    user = User()
+  user = User()
 
-    movie = Movie("Batman Begins", "Nolan", 2005, "Action")
+  movie = Movie("Batman Begins", "Nolan", 2005, "Action")
 
-    user.add_movie(movie)
-    user.remove_movie(0)
+  user.add_movie(movie)
+  user.remove_movie(0)
 
-    assert len(user) == 0
+  assert len(user) == 0
